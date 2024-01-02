6477ea412847ef25bb2fa94f8f7ec52df6a30edb
Harry Matson <150098079+HarryGelominaMatson@users.noreply.github.com>
2024-01-02 15:23:44 +0800

    Weblit
    basic lazy web project for medli

---
+++ App.svelte
@@ -0,0 +1,28 @@
+<script>
+	import Typewriter from '../Typewriter'
+</script>
+
+<Typewriter scramble>
+	<p>YOU HAVE BEEN HACKED	</p>
+</Typewriter>
+
+<style>
+	:global(*) { margin: 0; padding: 0 }
+	:global(body) {
+		background-color: black;
+		height: 100vh;
+		display: grid;
+		place-items: center;
+		cursor: default;
+	}
+	p {
+		font-family: monospace;
+		font-size: 4em;
+		color: rgb(30, 200, 30);
+		text-align: center;
+		padding: 0 30px;
+	}
+
+	@media (max-width: 700px) { p{ font-size: 3em } }
+	@media (max-width: 550px) { p{ font-size: 2em } }
+</style>
